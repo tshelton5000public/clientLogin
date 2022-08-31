4b5d72415725f3dbe8b5400f941ebe3a9d45de04
Tyler Shelton <tshelton@polarislabs.com>
2022-08-30 20:51:28 -0400

    initial commit, removing boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    Favorite Color: <input type="text" [formControl]="favoriteColorControl" />
+  `,
 })
 export class AppComponent {
-  title = 'clientLogin';
+  favoriteColorControl = new FormControl(' ');
 }
