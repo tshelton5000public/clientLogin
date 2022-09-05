4540eca8fa3562ebf6241fbb954e8cc1eda3bd4c
Tyler Shelton <tshelton@polarislabs.com>
2022-09-05 18:11:39 -0400

    fleshing out form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
-    Favorite Color: <input type="text" [formControl]="favoriteColorControl" />
+    <div class="centeredView">
+      <div class="form">
+        <h2 class="formTitle">Schnell-Schnell Login</h2>
+        <div class="input">
+          <span>Username: </span><input type="email" [formControl]="username" />
+          <div *ngIf="username.invalid && (username.dirty || username.touched)">
+            <span>Username must be in the form of an email address.</span>
+          </div>
+        </div>
+        <div class="input">
+          <span>Password:</span> <input type="text" [formControl]="password" />
+        </div>
+      </div>
+    </div>
   `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  favoriteColorControl = new FormControl(' ');
+  username: FormControl;
+  password: FormControl;
+
+  constructor() {
+    this.username = new FormControl('', Validators.email);
+    this.password = new FormControl('');
+  }
+
+  ngOnChanges() {
+    console.log('username', this.username);
+    console.log('password', this.password);
+  }
 }
